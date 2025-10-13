7c85b0b96ab8abcaeb6052d33810a5a8724be2c6
jw-nusqlab <jameswilliams.nusqlab@gmail.com>
2025-10-13 20:54:37 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        useLightningcss: false,
+    }
+};
+
+
 
 export default nextConfig;
